b9cfd8ab0249412bfd144a3adb3e0804d55ceefd
v1nc <github@reckendrees.systems>
2020-05-13 23:24:54 +0200

    init

--- update_lists.sh
+++ update_lists.sh
@@ -1,13 +1,15 @@
-dir_to_json(){
+
+mds_to_json(){
 	echo '{"docs": [' >$2
-	for dir in $1    
+	for md in $1/*.md    
 	do
-		dir=${dir%*/}  
-		if [ $dir != "assets" ] && [ $dir != "_data" ] && [ $dir != "node_modules" ] && [ $dir != "_layouts" ];then
-			printf '{"title": "%s","url":"%s"},' $dir $dir >>$2
+		name=$(echo $md | cut -d "/" -f2)
+		if [ $name != "README.md" ] ;then
+			url=$(echo $name | cut -d "." -f1 )
+			title=$(echo $url | tr '[:upper:]' '[:lower:]')
+			printf '{"title": "%s","url":"%s"},' $title "$1/$url.html" >>$2
 		fi
 	done
 	echo ']}' >>$2
 }
-
-dir_to_json "work/" _data/work.json
+mds_to_json "work" _data/work.json
