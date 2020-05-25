2065b0135e66fc942ddbc50055afe791c7c653c5
v1nc <github@reckendrees.systems>
2020-05-25 22:47:24 +0200

    add button onclick

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -1,6 +1,7 @@
 function copy(id) {
-  var copyText = document.getElementByName(id);
-  copyText.select();
-  copyText.setSelectionRange(0, 9999999999);
-  document.execCommand("copy");
+	console.log("copy",id);
+	var copyText = document.getElementsByName(id)[0];
+	copyText.select();
+	copyText.setSelectionRange(0, 9999999999);
+	document.execCommand("copy");
 } 
